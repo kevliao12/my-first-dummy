6c82a956b18aa5ddfc2e00fb1727bde93a1e3718
kevliao12 <kev.liao17@gmail.com>
2022-05-11 23:20:47 -0700

    DummyJSON app
    this took me way lnoger thani t should have

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
+import React,{ useState, useEffect } from 'react';
 
 function App() {
+  // state variable called images where everything will be stored plus React hook setImages using useState
+  const [images, setImages] = useState([]);
+
+  // create constant for API URL. Using async and await, API is called, and passed to Axios, which will parse data and 
+  // and store the response in a state variable.
+  const fetchImages = async() => {
+    const apiUrl = `https://dummyjson.com/products/1`;
+    const resp = await Axios(apiUrl);
+    setImages(resp.data.images)
+  }
+
+  // calls fetchImages 
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
+  // checks if the console is looking at the right thing (in this case, the images)
+  useEffect(() => {
+    console.log(images)
+  }, [images])
+
+
+  // iterate through array of images
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        images.map(image => (
+          <img src = {image} />
+        )
+        )}     
     </div>
   );
-}
+  }
 
-export default App;
+  export default App;
+
